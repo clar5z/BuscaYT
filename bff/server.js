4b6f140d533f2cc215180b1f59cc01be8b572f8e
Clarisse <clarisse.rib@outlook.com>
2024-07-04 22:51:45 -0300

    Corrige portas e Cors

--- bff/server.js
+++ bff/server.js
@@ -5,22 +5,28 @@ const request = require('request');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: '*', // Permite requisições de qualquer origem
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
 
 const proxy = (req, res, target) => {
   req.pipe(request(target + req.url)).pipe(res);
 };
 
 app.use('/mf_drawer', (req, res) => {
-  proxy(req, res, 'http://mf_drawer:8001');
+  proxy(req, res, 'http://mf_drawer:80');
 });
 
 app.use('/mf_videos', (req, res) => {
-  proxy(req, res, 'http://mf_videos:8002');
+  proxy(req, res, 'http://mf_videos:80');
 });
 
 app.get('/', (req, res) => {
-  res.send('<div>Servidor BFF</div>');
+  res.send('<div>Servidor BFF rodando</div>');
 });
 
 app.listen(PORT, () => {
