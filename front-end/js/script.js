d1ccf18d4273803b592691605049ac6fd9f87bdb
Clarisse <clarisse.rib@outlook.com>
2024-07-08 00:20:54 -0300

    Otimiza js do front-end

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,33 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("http://localhost:3000/mf_drawer")
-        .then((response) => response.text())
-        .then((data) => {
-            document.querySelector("#mf_drawer").innerHTML = data;
-            const script = document.createElement("script");
-            script.src = "http://localhost:3000/mf_drawer/js/script.js";
-            document.body.appendChild(script);
-            const style = document.createElement("link");
-            style.rel = "stylesheet";
-            style.href = "http://localhost:3000/mf_drawer/css/style.css";
-            document.body.appendChild(style);
-        })
-        .catch((error) => {
-            console.error("Erro ao carregar micro frontend:", error);
-        });
+    function loadMicroFrontend(name) {
+        fetch(`http://localhost:3000/${name}`)
+            .then((response) => response.text())
+            .then((data) => {
+                document.querySelector(`#${name}`).innerHTML = data;
+                const script = document.createElement("script");
+                script.src = `http://localhost:3000/${name}/js/script.js`;
+                document.body.appendChild(script);
+                const style = document.createElement("link");
+                style.rel = "stylesheet";
+                style.href = `http://localhost:3000/${name}/css/style.css`;
+                document.body.appendChild(style);
+            })
+            .catch((error) => {
+                console.error(`Erro ao carregar micro frontend ${name}:`, error);
+            });
+    }
 
-    fetch("http://localhost:3000/mf_videos")
-        .then((response) => response.text())
-        .then((data) => {
-            document.querySelector("#mf_videos").innerHTML = data;
-            const script = document.createElement("script");
-            script.src = "http://localhost:3000/mf_videos/js/script.js";
-            document.body.appendChild(script);
-            const style = document.createElement("link");
-            style.rel = "stylesheet";
-            style.href = "http://localhost:3000/mf_videos/css/style.css";
-            document.body.appendChild(style);
-        })
-        .catch((error) => {
-            console.error("Erro ao carregar micro frontend:", error);
-        });
+    loadMicroFrontend("mf_drawer");
+    loadMicroFrontend("mf_videos");
 });
